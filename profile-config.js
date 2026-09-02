ce92747f17ff461d2619f2c064f3cf0beffbab5b
phanthila1005 <phanthilakob@gmail.com>
2026-09-02 11:44:40 +0700

    Update STUDENT_PROFILE_WEB_APP_URL to new endpoint

--- profile-config.js
+++ profile-config.js
@@ -1,2 +1,2 @@
 window.STUDENT_PROFILE_WEB_APP_URL =
-  'https://script.google.com/macros/s/AKfycbzZPKCjqrvptWM7nVyquVLeU2GlKrS2UtljX1vQCqDHR2UVsL_-Tyh5BqVctKUwsv1C/exec';
+  'https://script.google.com/macros/s/AKfycbw3vEaydKEAWETe_JuTV6z04mO2rPC99buWi-PnJrs3Atinz5alLlM18HdeiuIofF0T/exec';
